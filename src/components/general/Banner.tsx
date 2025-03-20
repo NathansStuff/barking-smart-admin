2505fe1412c1f15f1a11669ed5cef88a46814e78
NathanDriveIq <nathan@driveiq.com.au>
2025-03-21 07:53:47 +1100

    hero

---
+++ src/components/general/Banner.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Banner(): React.JSX.Element {
+  return (
+    <div className='relative z-50 flex h-16 w-full items-center justify-center bg-[#ACADB6] text-white'>
+      <p>DISCOUNTED : Pet Enrichment Programs 20% OFF</p>
+    </div>
+  );
+}
+
+export default Banner;
