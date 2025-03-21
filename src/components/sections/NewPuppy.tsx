bf49956ab8670cafe27a044b54b446dab33f145a
NathansStuff <n.odonnell@hotmail.com>
2025-03-21 23:59:48 +1100

    scaling new puppy

--- src/components/sections/NewPuppy.tsx
+++ src/components/sections/NewPuppy.tsx
@@ -6,43 +6,50 @@ import { Button } from '../ui/button';
 
 function NewPuppy(): React.JSX.Element {
   return (
-    <section className='relative flex w-full flex-col items-center justify-center gap-4 overflow-hidden bg-[#A195AB] py-24'>
+    <section className='relative flex w-full flex-col items-center justify-center gap-2 overflow-hidden bg-[#A195AB] px-4 py-12 sm:gap-4 md:px-6 md:py-24'>
       <Image
         src='/assets/cloud_1.svg'
         alt=''
         width={851}
         height={114}
-        className='absolute left-[0.5px] top-0 w-1/2 opacity-60 mix-blend-multiply'
+        className='absolute left-0 top-0 w-[85vw] max-w-[800px] opacity-40 mix-blend-multiply sm:w-[60vw] sm:opacity-60 md:w-[50vw] lg:w-[45vw]'
       />
       <Image
         src='/assets/cloud_2.svg'
         alt=''
         width={375.76}
         height={182.45}
-        className='absolute left-1/2 top-0 w-1/4 -translate-x-1/2 opacity-80 mix-blend-overlay'
+        className='absolute left-1/2 top-0 w-[35vw] max-w-[400px] -translate-x-1/2 opacity-50 mix-blend-overlay sm:opacity-80 md:w-[25vw] lg:w-[20vw]'
       />
       <Image
         src='/assets/cloud_3.svg'
         alt=''
         width={375.76}
         height={182.45}
-        className='absolute bottom-0 left-[43%] w-1/4 -translate-x-1/2 opacity-70 mix-blend-overlay'
+        className='absolute bottom-0 left-1/2 w-[35vw] max-w-[400px] -translate-x-1/2 opacity-40 mix-blend-overlay sm:opacity-70 md:w-[25vw] lg:w-[20vw]'
       />
       <Image
         src='/assets/cloud_4.svg'
         alt=''
         width={375.76}
         height={182.45}
-        className='absolute -right-10 bottom-0 h-2/3 w-1/4 opacity-60 mix-blend-multiply'
+        className='absolute -right-[2vw] bottom-0 w-[35vw] max-w-[400px] translate-y-[20%] opacity-40 mix-blend-multiply sm:translate-y-0 sm:opacity-60 md:-right-10 md:w-[25vw] lg:w-[20vw]'
       />
-      <h2 className='z-10 text-5xl font-bold text-black'>
-        New <span className='text-yellow-400'>Puppy?</span> Start Off On The Right Paw
-      </h2>
-      <p className='z-10 pb-3 text-2xl text-black'>
-        Get A Free Sample Of Our Starter Puppy <br />
-        Enrichment Plan - Our Gift To You!
-      </p>
-      <Button className='z-10 bg-[#E4BD60] p-6 text-2xl text-white'> Download free program</Button>
+      <div className='container mx-auto flex max-w-[1400px] flex-col items-center'>
+        <h2 className='z-10 max-w-[90%] text-center text-2xl font-bold text-black sm:text-3xl md:max-w-none md:text-5xl'>
+          New <span className='text-yellow-400'>Puppy?</span> Start Off On The Right Paw
+        </h2>
+        <p className='z-10 max-w-[85%] pb-3 text-center text-lg text-black sm:text-xl md:max-w-none md:text-2xl'>
+          Get A Free Sample Of Our Starter Puppy{' '}
+          <span className='hidden sm:inline'>
+            <br />
+          </span>
+          Enrichment Plan - Our Gift To You!
+        </p>
+        <Button className='z-10 bg-[#E4BD60] px-6 py-3 text-lg text-white sm:text-xl md:p-6 md:text-2xl'>
+          Download free program
+        </Button>
+      </div>
     </section>
   );
 }
