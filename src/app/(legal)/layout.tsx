f3f7cba6119e38bab52f6f4a3f60735672868bfc
NathansStuff <n.odonnell@hotmail.com>
2025-03-23 17:40:27 +1100

    lint

--- src/app/(legal)/layout.tsx
+++ src/app/(legal)/layout.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import Footer from '@/components/sections/Footer';
 import Header from '@/components/sections/Header';
-
 function Layout({ children }: { children: React.ReactNode }): React.JSX.Element {
   return (
     <>
--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 function Header(): React.JSX.Element {
-  return (
-    <div>Header</div>
-  )
+  return <div>Header</div>;
 }
 
-export default Header
+export default Header;
